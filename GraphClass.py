fc00cd87cc8a30751ff99277d1c746efebd76a47
Fstalker-debug <134922311+Fstalker-debug@users.noreply.github.com>
2024-07-22 19:24:58 +0300

    added display graph method

--- GraphClass.py
+++ GraphClass.py
@@ -2,18 +2,34 @@ class graph:
     
     def __init__(self):
 
-        self.node = {} # i will store here users
+        self.vertices = {} # i will store here users
         self.graph = []
 
-    def addUsertoGraph(self, node):
+    def addUsertoGraph(self, vertex):
 
-        if node not in self.node:
+        if vertex not in self.vertices:
 
-            self.node[node] = len(self.graph)
+            self.vertices[vertex] = len(self.graph)
 
             for row in self.graph:
                 row.append(0)
-            self.graph.append([0]*(len(self.node)+1))
+            self.graph.append([0]*(len(self.graph)+1))
+
+    def DisplayGraph(self):
+
+        vertices = sorted(self.vertices.keys())
+        matrix = [[0]*len(vertices) for _ in range(len(vertices))]
+
+        for i, row in enumerate(self.graph):
+            for j, value in enumerate(row):
+                matrix[i][j] = value
+
+        print("  " + " ".join(vertices))
+        for i, row in enumerate(matrix):
+            print(vertices[i] + "|" + " ".join(map(str, row)))
+        
+            
+
 
 #####################################################################################
 # TESTING LAB###
@@ -22,3 +38,8 @@ class graph:
 graph = graph()
 graph.addUsertoGraph("User1")
 graph.addUsertoGraph("User2")
+graph.addUsertoGraph("User3")
+graph.addUsertoGraph("User4")
+graph.addUsertoGraph("User5")
+
+graph.DisplayGraph()
