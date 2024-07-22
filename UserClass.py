9ffd1b71c59842f081ff68334d6360d3d9f14906
Fstalker-debug <134922311+Fstalker-debug@users.noreply.github.com>
2024-07-22 23:33:06 +0300

    added the attribute list of friends to my class

--- UserClass.py
+++ UserClass.py
@@ -4,20 +4,25 @@ class user:
     
     username_log = []
 
-    def __init__(self, firstName=None, lastName=None, username=None, ID=None, email=None, Bio=None)->None:
+    def __init__(self, firstName=None, lastName=None, username=None, ID=None, email=None, Bio=None, FriendList=None)->None:
+        if FriendList is None:
+            FriendList = []
         self.firstName = firstName
         self.lastName = lastName
         self.username = username
         self.ID = ID
         self.email = email
         self.Bio = Bio
-        self.FriendList = []
+        self.FriendList = FriendList
 
         if username is not None:
             user.username_log.append(username)
         else:
             raise ValueError(f"username: {username} is already taken")
         pass
+
+    def showFriends(self):
+        return f"{self.firstName} friend list is {self.FriendList}"
 
     def displayUser(self):
 
@@ -37,37 +42,47 @@ class user:
         self.ID = input("Please enter your ID: ")
         self.email = input("Please enter your email: ")
         self.Bio = input("Please enter your Bio: ")
+        ## later on i need to check i should allow to add the username of Friends from here
     
     @classmethod
     def getListUsers(cls):
 
         return cls.username_log
     
-    def addFriend(self, user1, user2):
+    def addFriend(self, user_name):
         
-        if user1 not in self.FriendList and user2 not in self.FriendList:
+        if user_name not in self.FriendList:
             
-            self.FriendList.append(user2)
+            self.FriendList.append(user_name)
 
     def rmvFriend(self, user1, user2):
 
         if user1 in self.FriendList and user2 in self.FriendList:
             pass
 
-    def userFriendList(self, username):
-
-        if username in self.username_log:
-            return self.FriendList() ## will it give the list of Friends just for this username
-        pass
+    def printfriends(self):
+            
+            return self.FriendList ## will it give the list of Friends just for this username
+        
 
 ################################################
 ## TESTING ###########                          #
 ################################################
 
-user1 = user("bob","james","bob_james","B283","bob_james@gmail.com","Hello world")
-user2 = user("lolipop","lebron","lebron_lolipop","B293","lebron_lolipop@gmail.com","HI lolo world")
-user3 = user("Brian","kpop","brian_kpop","B285","brian_kpop@gmail.com","I am sexy")
-user4 = user("Smith","kurky","smith_kurky","B383","smith_kurky@gmail.com","Hola world")
-user5 = user("Barron","Trump","Barron Tr","B283","Barron_trump@hotmail.com","I am the president of the united states of America")
+user1 = user("bob","james","bob_james","B283","bob_james@gmail.com","Hello world",["lebron_lolipop"])
+user2 = user("lolipop","lebron","lebron_lolipop","B293","lebron_lolipop@gmail.com","HI lolo world",["smith_kurky"])
+user3 = user("Brian","kpop","brian_kpop","B285","brian_kpop@gmail.com","I am sexy",["Barron Tr"])
+user4 = user("Smith","kurky","smith_kurky","B383","smith_kurky@gmail.com","Hola world",["bob_james"])
+user5 = user("Barron","Trump","Barron Tr","B283","Barron_trump@hotmail.com","I am the president of the united states of America",["brian_kpop"])
 
 print(user5.displayUser())
+
+user1.addFriend(user2.username)
+
+print(user1.FriendList) ## because i am printing it directly i am not getting the result i want
+
+print(user1.showFriends())
+# user1.addFriend(user2.username) => doesn't work
+user1.addFriend("brian_kpop")
+
+print(user1.showFriends())
