2ee51d43f32f1adc9a4648ce3e1f300e2c242c2f
Fstalker-debug <134922311+Fstalker-debug@users.noreply.github.com>
2024-07-22 23:42:01 +0300

    modified graph class

--- GraphClass.py
+++ GraphClass.py
@@ -2,44 +2,29 @@ class graph:
     
     def __init__(self):
 
-        self.vertices = {} # i will store here users
-        self.graph = []
+        self.connections = []
+        self.graph = {}
 
-    def addUsertoGraph(self, vertex):
+    def addConnection(self, user, list):
 
-        if vertex not in self.vertices:
+        self.graph[user] = list
 
-            self.vertices[vertex] = len(self.graph)
+    def displayConnections(self):
 
-            for row in self.graph:
-                row.append(0)
-            self.graph.append([0]*(len(self.graph)+1))
-
-    def DisplayGraph(self):
-
-        vertices = sorted(self.vertices.keys())
-        matrix = [[0]*len(vertices) for _ in range(len(vertices))]
-
-        for i, row in enumerate(self.graph):
-            for j, value in enumerate(row):
-                matrix[i][j] = value
-
-        print("  " + " ".join(vertices))
-        for i, row in enumerate(matrix):
-            print(vertices[i] + "|" + " ".join(map(str, row)))
-        
-            
+        return self.graph
 
 
-#####################################################################################
-# TESTING LAB###
-#####################################################################################
 
-graph = graph()
-graph.addUsertoGraph("User1")
-graph.addUsertoGraph("User2")
-graph.addUsertoGraph("User3")
-graph.addUsertoGraph("User4")
-graph.addUsertoGraph("User5")
 
-graph.DisplayGraph()
+#####
+# testing
+###########
+
+network_graph = graph()
+
+network_graph.addConnection("user1" , ["user2" , "user3"])
+network_graph.addConnection("user2" , ["user1" , "user4"])
+network_graph.addConnection("user5" , ["user6" , "user13"])
+
+print(network_graph.displayConnections())
+
