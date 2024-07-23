9e531300f7249ce569ac02f3eb9ff193b1298510
Fstalker-debug <134922311+Fstalker-debug@users.noreply.github.com>
2024-07-24 01:53:28 +0300

    created a binary seach algorithm

--- additionalfct.py
+++ additionalfct.py
@@ -47,12 +47,68 @@ print(sorted_usernames_by_username)
 ##### searching algorithm 
 user_ll = [testuser,testuser1,testuser2,testuser3,testuser4]
 
-def search(lst , attribute):
+def search1(lst , attribute):
 
     for i in range(len(user_ll)):
         x = user_ll[i]
         if x.username == attribute:
             return x.displayUser()
         
-result = search(user_ll, "smith_kurky")
-print(result)
+# result = search1(user_ll, "smith_kurky")
+# print(result)
+
+# Create user instances
+testuser = user("james", "bond", "james_bond", "B1810", "james.bond@gmail.com", "I am the king", ["Barron Tr"])
+testuser1 = user("lolipop", "lebron", "lebron_lolipop", "B293", "lebron_lolipop@gmail.com", "HI lolo world", ["smith_kurky"])
+testuser2 = user("Brian", "kpop", "brian_kpop", "B285", "brian_kpop@gmail.com", "I am sexy", ["Barron Tr"])
+testuser3 = user("Smith", "kurky", "smith_kurky", "B383", "smith_kurky@gmail.com", "Hola world", ["bob_james"])
+testuser4 = user("Barron", "Trump", "Barron Tr", "B283", "Barron_trump@hotmail.com", "I am the president of the united states of America", ["brian_kpop"])
+
+sortedlist = [testuser4,testuser2,testuser1,testuser3,testuser]
+
+def search2(lst, attribute, value):
+
+    low = 0 
+    high = len(lst)
+
+    while high >= low:
+        mid = (high + low)//2
+
+        if getattr(lst[mid], attribute) == value:
+            
+            return lst[mid]
+        
+        elif getattr(lst[mid], attribute) < value:
+
+            low = mid + 1
+        
+        else:
+
+            high = mid - 1
+
+    return None
+
+# def search2(lst, attribute, value):
+#     low = 0 
+#     high = len(lst) - 1
+
+#     while high >= low:
+#         mid = (high + low) // 2
+#         mid_value = getattr(lst[mid], attribute)
+
+#         if mid_value == value:
+#             return lst[mid]
+#         elif mid_value < value:
+#             low = mid + 1
+#         else:
+#             high = mid - 1
+
+#     return None  # If the value is not found
+
+result = search2(sortedlist, "ID", "B283")
+
+if result:
+    print(f"Found user:\n{result.displayUser()}")
+else:
+    print("User not found.")
+
