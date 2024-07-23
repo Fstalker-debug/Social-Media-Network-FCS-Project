bf53fe843f442b00bd87f5b83ca33305b13e7c38
Fstalker-debug <134922311+Fstalker-debug@users.noreply.github.com>
2024-07-23 19:22:07 +0300

    added a method that remove connections between users

--- Application.py
+++ Application.py
@@ -3,21 +3,42 @@ from UserClass import user
 from GraphClass import graph 
 
 NetworkGraph = graph()
+
 user1 = user("bob","james","bob_james","B283","bob_james@gmail.com","Hello world",["lebron_lolipop","Barron Tr"])
 user2 = user("lolipop","lebron","lebron_lolipop","B293","lebron_lolipop@gmail.com","HI lolo world",["smith_kurky"])
 user3 = user("Brian","kpop","brian_kpop","B285","brian_kpop@gmail.com","I am sexy",["Barron Tr"])
 user4 = user("Smith","kurky","smith_kurky","B383","smith_kurky@gmail.com","Hola world",["bob_james"])
 user5 = user("Barron","Trump","Barron Tr","B283","Barron_trump@hotmail.com","I am the president of the united states of America",["brian_kpop"])
+
 ## important to note that i should always be passing the username of the user
-NetworkGraph.addConnection("bob_james", user1.FriendList)
-NetworkGraph.addConnection("lebron_lolipop", user2.FriendList)
+
+## 1) add users to the graph (using their username)
+
+NetworkGraph.addUserToGraph(user1.username)
+NetworkGraph.addUserToGraph(user2.username)
+NetworkGraph.addUserToGraph(user3.username)
+NetworkGraph.addUserToGraph(user4.username)
+NetworkGraph.addUserToGraph(user5.username)
+
+## 2) create a connection between two users (using also their username)
+
+NetworkGraph.addConnection(user1.username, user2.username)
+NetworkGraph.addConnection(user1.username, user3.username)
+NetworkGraph.addConnection(user1.username, user4.username)
+
+NetworkGraph.addConnection(user4.username, user5.username)
 
 connections = NetworkGraph.displayConnections()
+
 print(connections)
+
+NetworkGraph.rmvConnection(user1.username,user3.username)
+
+print(NetworkGraph.displayConnections())
 
 ## testing 
 
-user1.addFriend("smith_kurky")
-user1.addFriend("Alexandro007")
+# user1.addFriend("smith_kurky")
+# user1.addFriend("Alexandro007")
 
-print(connections)
+# print(connections)
--- GraphClass.py
+++ GraphClass.py
@@ -2,12 +2,27 @@ class graph:
     
     def __init__(self):
 
-        self.connections = []
+        self.vertices = []
         self.graph = {}
 
-    def addConnection(self, user, list):
+    def addUserToGraph(self, user):
 
-        self.graph[user] = list
+        if user not in self.vertices:
+            self.vertices.append(user)
+            self.graph[user] = []
+
+    def addConnection(self, user1, user2):
+        
+        if user1 in self.vertices and user2 in self.vertices:
+
+            self.graph[user1].append(user2)
+        
+
+    def rmvConnection(self,user1, user2):
+
+        if user1 in self.vertices and user2 in self.vertices:
+            self.graph[user1].remove(user2)
+        pass
 
     def displayConnections(self):
 
@@ -16,15 +31,20 @@ class graph:
 
 
 
-#####
-# testing
+###########
+# testing #
 ###########
 
 network_graph = graph()
-
-network_graph.addConnection("user1" , ["user2" , "user3"])
-network_graph.addConnection("user2" , ["user1" , "user4"])
-network_graph.addConnection("user5" , ["user6" , "user13"])
+## add users to the graph (adding vertices)
+network_graph.addUserToGraph("user1")
+network_graph.addUserToGraph("user2")
+network_graph.addUserToGraph("user3")
+network_graph.addUserToGraph("user4")
+## add connections/edges between users
+network_graph.addConnection("user1" , "user2")
+network_graph.addConnection("user2" ,"user3")
+network_graph.addConnection("user2" ,"user4")
 
 print(network_graph.displayConnections())
 
