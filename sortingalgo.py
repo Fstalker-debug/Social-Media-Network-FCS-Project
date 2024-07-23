8399717d1f33685b2688849d9084e61bd75a9c65
Fstalker-debug <134922311+Fstalker-debug@users.noreply.github.com>
2024-07-24 02:21:31 +0300

    added a new sorting algorithm

---
+++ sortingalgo.py
@@ -0,0 +1,30 @@
+from UserClass import user
+
+
+testuser = user("james", "bond", "james_bond", "A1810", "james.bond@gmail.com", "I am the king", ["Barron Tr"])
+testuser1 = user("lolipop", "lebron", "lebron_lolipop", "B293", "lebron_lolipop@gmail.com", "HI lolo world", ["smith_kurky"])
+testuser2 = user("Brian", "kpop", "brian_kpop", "B285", "brian_kpop@gmail.com", "I am sexy", ["Barron Tr"])
+testuser3 = user("Smith", "kurky", "smith_kurky", "B383", "smith_kurky@gmail.com", "Hola world", ["bob_james"])
+testuser4 = user("Barron", "Trump", "Barron Tr", "B283", "Barron_trump@hotmail.com", "I am the president of the united states of America", ["brian_kpop"])
+
+ltl = [testuser1, testuser, testuser2, testuser3, testuser4]
+
+def easy_sorting(lst, firstName):
+    tnt = []
+    for i in range(len(lst)):
+        temp = lst[i].firstName
+        tnt.append(temp)
+
+    return sorted(tnt)
+
+print(easy_sorting(ltl, "firstName"))
+
+
+def ID_sorting(lst, ID):
+    tnt = []
+    for i in range(len(lst)):
+        temp = lst[i].ID
+        tnt.append(temp)
+    return sorted(tnt)
+
+print(ID_sorting(ltl, "ID"))
